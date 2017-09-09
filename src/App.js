6b199eb99c39f76b15ccb1abfb3f909231261a80
Krisofer Brink <kpgbrink@gmail.com>
2017-09-09 18:44:44 -0400

    got rid of warning

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import GamePage from './pages/GamePage';
-import AdminPage from './pages/AdminPage';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-
-
   pageSwitcher() {
     return (
       <Switch>
